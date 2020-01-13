cd802710cb829b73ec3fff55dffc60dbab41271b
saravanakumar <tns1695@gmail.com>
2020-01-13 14:17:23 +0530

    Updated navbar for authenticated users

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const hbs = require('hbs');
+const session = require("express-session");
 const app = express();
 
 const homepageRoutes = require('./routes/homepage.route');
 app.use(express.static('public'));
 app.listen(6060);
-app.set("view engine","hbs")
+app.set("view engine","hbs");
+app.use(session({
+    secret: "r545hg",
+    cookie: {
+        maxAge: 1000 * 6000,
+        path: "/",
+        httpOnly: true
+    }
+}));
 app.use(homepageRoutes);
--- controller/homepage.controller.js
+++ controller/homepage.controller.js
@@ -1,6 +1,13 @@
 
 var getHomepage = function (req,res) {
-    res.render('homepage');
+    //req.session.user = {name: "Saravanakumar T N", image: "https://accounts.practo.com/profile_picture/9936369/medium_thumbnail"};
+    if (req.session.user) {
+        res.render('homepage',{user: req.session.user, loggedin: true});
+    } else{
+        res.render('homepage', {loggedin: false});
+    }
+    
 }
+
 
 module.exports = {getHomepage};
