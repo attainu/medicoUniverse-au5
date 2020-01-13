bc55336bd025b49ec1cde548530cbabe421cfff1
saravanakumar <tns1695@gmail.com>
2020-01-13 17:59:58 +0530

    Updated with minor changes

--- controller/homepage.controller.js
+++ controller/homepage.controller.js
@@ -2,9 +2,9 @@
 var getHomepage = function (req,res) {
     //req.session.user = {name: "Saravanakumar T N", image: "https://accounts.practo.com/profile_picture/9936369/medium_thumbnail"};
     if (req.session.user) {
-        res.render('homepage',{user: req.session.user, loggedin: true});
+        res.render('homepage',{user: req.session.user});
     } else{
-        res.render('homepage', {loggedin: false});
+        res.render('homepage');
     }
     
 }
