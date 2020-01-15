e83d9321dbf07d398bbe1d48bba038025ef4e780
saravanakumar <tns1695@gmail.com>
2020-01-15 15:15:05 +0530

    updated dashboard with profile section

--- controller/dashboard.controller.js
+++ controller/dashboard.controller.js
@@ -1,7 +1,7 @@
 
 
 var getDashboard = function (req,res) {
-    req.session.user = {name: "Saravanakumar", image: "./image/mypic.jpg",phone: "986543210", email: "tns1695@gmail.com"};
+    req.session.user = {name: "Saravanakumar",phone: "986543210", email: "tns1695@gmail.com"};
     res.render('dashboard',{user: req.session.user});
 }
 
---
+++ public/js/dashboard.js
@@ -0,0 +1,8 @@
+var he = function () {
+    setTimeout(function () {
+        var val =document.getElementById('displaypicture').value
+        document.getElementById('dp').setAttribute('src', val)
+    }, 10000)
+    
+}
+
